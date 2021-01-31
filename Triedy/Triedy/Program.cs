3f295d216173ccb7515e49c501d11e73e655408d
BaskaKlim <baska.klimekova@gmail.com>
2021-01-31 22:30:57 +0100

    creating new instance of Book class

--- Triedy/Triedy/Program.cs
+++ Triedy/Triedy/Program.cs
@@ -6,7 +6,13 @@ namespace Triedy
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            Book book1 = new Book();
+
+            book1.Title = "Príbeh štúdia Pixar";
+            book1.Author = "Lawrence Levy";
+            book1.ISBN = "9788081820700";
+
+            Console.WriteLine(book1.Title);
         }
     }
 }
