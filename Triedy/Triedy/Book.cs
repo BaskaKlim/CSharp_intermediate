b90859383ff99e263bc78b60116671dfc22f1282
BaskaKlim <baska.klimekova@gmail.com>
2021-01-31 22:26:25 +0100

    adding simple Class Book wotch constructor and properties

---
+++ Triedy/Triedy/Book.cs
@@ -0,0 +1,24 @@
+﻿using System;
+namespace Triedy
+{
+    public class Book
+    {
+        private int ID;
+        public string Title;
+        public string Author;
+        public string ISBN;
+
+        public Book()
+        {
+          
+        }
+
+        public Book(int ID, string ISBN, string Title, string Author)
+        {
+            this.ID = ID;
+            this.ISBN = ISBN;
+            this.Title = Title;
+            this.Author = Author;
+        }
+    }
+}
