a3bee67a83cabc88989700560deafda8eb6d75ed
BaskaKlim <baska.klimekova@gmail.com>
2021-01-31 22:20:38 +0100

    Triedy exercise init structure

---
+++ Triedy/Triedy/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace Triedy
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
