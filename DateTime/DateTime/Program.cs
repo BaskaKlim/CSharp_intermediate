4f8dc7a727ded204280a038484fb40860f340951
BaskaKlim <baska.klimekova@gmail.com>
2021-02-07 16:19:07 +0100

    Diferent Time and Date formats

--- DateTime/DateTime/Program.cs
+++ DateTime/DateTime/Program.cs
@@ -27,7 +27,16 @@ namespace DateAndTime
          List<double> myAge = HowOldIAm(now,startDay);
 
          Console.WriteLine($"Mas presne {myAge[0]} rokov. Narodila si sa pred {myAge[1]} dnami, {myAge[2]} hodinami a {myAge[3]} minutami.");
-            
+
+
+            //rozne formaty casu a datumu
+            var teraz = DateTime.Now;
+            Console.WriteLine(now.ToLongDateString());  // Sunday, February 7, 2021
+            Console.WriteLine(now.ToLongTimeString());  // 4:17:01 PM
+            Console.WriteLine(now.ToShortDateString()); // 2/7/2021
+            Console.WriteLine(now.ToShortTimeString()); // 4:17 PM
+            Console.WriteLine(now.ToUniversalTime());   // 2/7/2021 3:17:01 PM
+
         }
     }
 }
